e60fd06e66c281893666aacfa1e70d11cc88497e
Viktor Iovenko <iovenkovn@gmail.com>
2026-02-04 15:22:03 +0100

    обновил

--- constants.py
+++ constants.py
@@ -1,8 +1,7 @@
-﻿WATCH_ROOT = "/mnt/external"
-DRONE_DEST = "/mnt/drone"      # Папка назначения DRONE
-POCKET_DEST = "/mnt/pocket"    # Папка назначения POCKET
-FOTO_DEST = "/mnt/foto"    
-
+﻿WATCH_PATH = r"/mnt/external"
+DRONE_DEST = r"/mnt/drone"
+POCKET_DEST = r"/mnt/pocket"
+FOTO_DEST = r"/mnt/foto"
 
 #WATCH_PATH = r"\\share\\external"
 #DRONE_DEST = r"\\share\\CACHEDEV3_DATA\\DRONE"
